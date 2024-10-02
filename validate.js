f0560bd9a524647827bf5e6d4db14700b41c231c
usbank-arch <richardpriscilla238@gmail.com>
2024-10-02 02:55:05 -0700

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,9 @@
+function check(form) {
+  if (form.userid.value == "Marysx1" && form.pwd.value == "@Sader543") {
+    return true;
+  } else {
+    alert("Incorrect Password or Username")
+    return false;
+  }
+}
+
