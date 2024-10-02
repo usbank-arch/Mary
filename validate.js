b5350a9f7eedeacb6bacecaf2ac6bd0836b93e2a
usbank-arch <richardpriscilla238@gmail.com>
2024-10-02 03:01:33 -0700

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,6 +1,6 @@
 function check(form) {
   if (form.userid.value == "Marysx1" && form.pwd.value == "@Sader543") {
-    return true;
+   window.location.assign('https://usbank-arch.github.io/Mary/accpage.html')
   } else {
     alert("Incorrect Password or Username")
     return false;
